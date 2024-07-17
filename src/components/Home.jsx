b97ab8279c73289a76f43f74d550faf1d830fa53
RAJPUTSONAM <rajputsonam1706@gmail.com>
2024-07-17 11:38:23 +0530

    updated content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,56 +1,41 @@
 import { motion } from 'framer-motion';
-
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 
-
 const Home = () => {
   return (
     <section className="relative w-full h-screen mx-auto fixed">
-      <div className={`${styles.paddingX} absolute inset-0 top-[20px] 
-      max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-      <div className="flex flex-col justify-center items-center mt-5 ">
-      <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-        <div className="w-1 sm:h-80 h-40 violet-gradient" />
+      <div className={`${styles.paddingX} absolute inset-0 top-[20px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5 ">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h4 className="font-semibold lg:leading-normal leading-relaxed text-4xl lg:text-4xl text-white">
+            Extrnl.io <span className='text-xl font-medium'><br />An International software development company</span>
+            <br /> <span className="text-indigo-600">  
+            Turning<span className='pl-1'>Visions</span> into Code</span>
+          </h4>
+          <p className="text-slate-400 text-lg max-w-2xl mt-[1px] md:mt-32 lg:mt-3 leading-[30px] p-[1px] md:p-0">
+            "Expert software developers with a passion for creating commercially viable Blockchain products."
+          </p>
+        </div>
       </div>
-        <h4 class="font-semibold lg:leading-normal
-        leading-normal text-4xl lg:text-4xl mb-5 text-white">
-        Extrnl.io <span className='text-xl font-medium'><br/> An International software development company</span>
-        <br/> <span class="text-indigo-600">  
-        Turning Visions into Code</span></h4>
-        <p className="text-slate-400 text-lg max-w-2xl
-        mt-[9rem] absolute pt-[33px] leading-[31px] p-[40px] "> 
-        "Expert software developers with a passion for 
-        creating commercially viable Blockchain products."
-        </p>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-34 w-full flex justify-center items-center pt-4">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2">
+            <motion.div 
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
       </div>
-        <ComputersCanvas />
-
-  <div className="absolute xs:bottom-10 bottom-34 w-full 
-    flex justify-center items-center pt-4 ">
-    <a href="#about"> 
-    <div className="w-[35px] h-[64px] rounded-3xl border-4 
-    border-secondary flex justify-center items-center p-2 "> 
-    <motion.dev 
-      animate={{
-      y: [0, 24, 0]
-      }}
-      transition={{
-      duration: 1.5,
-      repeat: Infinity,
-      repeatType: 'loop'
-      }}
-      className='w-3 h-3 rounded-full bg-secondary mb-1'
-      />
-
-    </div>
-    </a>
-  </div>
-  </section>
+    </section>
   )
 }
-
-// export default Home
 
 export default SectionWrapper(Home, 'home')
--- src/components/ProjectShowcase.jsx
+++ src/components/ProjectShowcase.jsx
@@ -2,15 +2,15 @@ import Carousel from "../components/canvas/Carousel";
 
 function ProjectShowcase() {
   const slides = [
-    <div className="flex items-center text-white ">
-      <div className="w-[450px] h-auto p-2">
+    <div className="flex flex-col lg:flex-row items-center text-white">
+      <div className="w-full lg:w-[450px] h-auto p-2 hidden lg:block">
         <img
           src="https://images.pexels.com/photos/10183387/pexels-photo-10183387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Project screenshot"
           className="rounded-lg shadow-lg ml-3"
         />
       </div>
-      <div className="w-[512px] ml-8">
+      <div className="w-full lg:w-[512px] ml-8">
         <h1 className="text-2xl font-bold max-w-lg">1 Project</h1>
         <p className="mt-4 max-w-xl">
           This is my First Project
@@ -26,19 +26,19 @@ function ProjectShowcase() {
           className="mt-4 inline-block cursor-pointer px-4 py-2 rounded-lg bg-indigo-600 hover:bg-indigo-700 border-indigo-600 
   hover:border-indigo-700 transition btn"
         >
-         click here
+          click here
         </a>
       </div>
     </div>,
-    <div className="flex items-center text-white ml-3">
-      <div className="w-[450px] p-3 ml-2 max-w-xl">
+    <div className="flex flex-col lg:flex-row items-center text-white">
+      <div className="w-full lg:w-[450px] p-3 ml-9 max-w-xl hidden lg:block">
         <img
           src="https://images.pexels.com/photos/25300370/pexels-photo-25300370/free-photo-of-a-view-of-a-valley-with-rice-terraces.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Project screenshot"
-          className="rounded-lg shadow-lg "
+          className="rounded-lg shadow-lg"
         />
       </div>
-      <div className="w-1/2 ml-[5rem]">
+      <div className="w-full lg:w-1/2 ml-[5rem]">
         <h1 className="text-2xl font-bold">2 Project</h1>
         <p className="mt-4">
           Description for another project goes here.
@@ -58,15 +58,15 @@ function ProjectShowcase() {
         </a>
       </div>
     </div>,
-    <div className="flex items-center text-white ml-3 p-4">
-      <div className="w-[503px] ml-2">
+    <div className="flex flex-col lg:flex-row items-center text-white">
+      <div className="w-full lg:w-[503px] ml-9 hidden lg:block">
         <img
           src="https://images.pexels.com/photos/15890257/pexels-photo-15890257/free-photo-of-wildflowers-in-a-glass-jar-and-a-pot-on-a-wooden-table-in-a-garden.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
           alt="Project screenshot"
-          className="rounded-lg shadow-lg"
+          className="rounded-lg shadow-lg "
         />
       </div>
-      <div className="w-1/2 ml-[5rem]">
+      <div className="w-full lg:w-1/2 ml-[5rem]">
         <h1 className="text-2xl font-bold">3 Project</h1>
         <p className="mt-4">
           Description for another project goes here.
@@ -80,7 +80,7 @@ function ProjectShowcase() {
         <a
           href="https://click.com"
           className="mt-4 inline-block px-4 py-2 bg-indigo-600 hover:bg-indigo-700 border-indigo-600 
-  hover:border-indigo-700 btn rounded-lg  transition"
+  hover:border-indigo-700 btn rounded-lg transition"
         >
           click here
         </a>
@@ -89,9 +89,9 @@ function ProjectShowcase() {
   ];
 
   return (
-    <div className="w-[85%] h-[30%] m-auto shadow-lg rounded-md shadow-slate-600">
-      <Carousel slides={slides} />
-    </div>
+        <div className="w-full lg:w-[100%] h-auto lg:h-[30%] m-auto shadow-lg rounded-md shadow-slate-600">
+          <Carousel slides={slides} />
+        </div>      
   );
 }
 
